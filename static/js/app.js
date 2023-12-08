84894284dbfb7ac65e5c770b7acab6a5c0ff7a77
DayanaIm <dayana.imanova.cnd@gmail.com>
2023-12-07 20:58:03 -0500

    app3

--- static/js/app.js
+++ static/js/app.js
@@ -129,11 +129,10 @@ function renderChart(data) {
 // Second Visual//
 //--------------//
 
-//---------------------------------------------------------
-// // Fetch data from Flask API and create a dropdown
-//---------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
-    
+
+    let chart2;
+
     fetch('/api/suicides_data')
         .then(response => response.json())
         .then(data => {
@@ -141,73 +140,84 @@ document.addEventListener("DOMContentLoaded", function () {
             const years = Array.from(new Set(data.map(item => item.year)));
 
             // Create a dropdown for the country filter
-            const countryDropdown = document.createElement('select');
-            countryDropdown.id = 'countryDropdown';
-            countryDropdown.addEventListener('change', updateChart);
-
-            // Populate the dropdown with country options
-            countries.forEach(country => {
-                const option = document.createElement('option');
-                option.value = country;
-                option.text = country;
-                countryDropdown.add(option);
-            });
+            const countryDropdown = createDropdown('countryDropdown', countries, updateChart);
 
             // Create a dropdown for the year filter
-            const yearDropdown = document.createElement('select');
-            yearDropdown.id = 'yearDropdown';
-            yearDropdown.addEventListener('change', updateChart);
-
-            // Populate the dropdown with year options
-            years.forEach(year => {
-                const option = document.createElement('option');
-                option.value = year;
-                option.text = year;
-                yearDropdown.add(option);
-            });
+            const yearDropdown = createDropdown('yearDropdown', years, updateChart);
 
             document.getElementById('graph2').appendChild(countryDropdown);
             document.getElementById('graph2').appendChild(yearDropdown);
 
             // Initial rendering of the chart with all data
-            renderChart(data);
+            renderChart2(data);
         })
         .catch(error => console.error('Error fetching data:', error));
 
-});
+    //---------------------------------------------------------
+    // Fetch data from Flask API based on the selected country
+    //---------------------------------------------------------
+    function updateChart() {
+        const selectedCountry = document.getElementById('countryDropdown').value;
+        const selectedYear = document.getElementById('yearDropdown').value;
 
-
-//---------------------------------------------------------
-// Fetch data from Flask API based on the selected country
-//---------------------------------------------------------
-function updateChart() {
-    const selectedCountry = document.getElementById('countryDropdown').value;
-
-    fetch(`/api/suicides_data?country=${selectedCountry}`)
-        .then(response => response.json())
-        .then(data => {
-            // Destroy existing chart if any
-            if (chart) {
-                chart.destroy();
-            }
-
-            // Render the new chart
-            renderChart(data);
-        })
-        .catch(error => console.error('Error fetching data:', error));
-
-        
-    const selectedYear = document.getElementById('yearDropdown').value;
-    fetch(`/api/suicides_data?year=${selectedYear}`)
+        fetch(`/api/suicides_data?country=${selectedCountry}&year=${selectedYear}`)
             .then(response => response.json())
             .then(data => {
                 // Destroy existing chart if any
-                if (chart) {
-                    chart.destroy();
+                if (chart2) {
+                    chart2.destroy();
                 }
-    
+
                 // Render the new chart
-                renderChart(data);
+                renderChart2(data);
             })
             .catch(error => console.error('Error fetching data:', error));
     }
+
+    //---------------------------------------------------------
+    // Render a pie chart with total "suicides_no" for male and female
+    //---------------------------------------------------------
+    function renderChart2(data) {
+        const maleData = data.filter(item => item.sex === 'male');
+        const femaleData = data.filter(item => item.sex === 'female');
+
+        const maleSuicides = maleData.reduce((total, item) => total + item.suicides_no, 0);
+        const femaleSuicides = femaleData.reduce((total, item) => total + item.suicides_no, 0);
+
+        const ctx = document.getElementById('chart2').getContext('2d');
+        chart2 = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Male', 'Female'],
+                datasets: [{
+                    data: [maleSuicides, femaleSuicides],
+                    backgroundColor: ['blue', 'pink'],
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Total Suicides by Gender'
+                }
+            }
+        });
+    }
+
+    //---------------------------------------------------------
+    // Helper function to create a dropdown
+    //---------------------------------------------------------
+    function createDropdown(id, options, onChange) {
+        const dropdown = document.createElement('select');
+        dropdown.id = id;
+        dropdown.addEventListener('change', onChange);
+
+        options.forEach(option => {
+            const optionElement = document.createElement('option');
+            optionElement.value = option;
+            optionElement.text = option;
+            dropdown.add(optionElement);
+        });
+
+        return dropdown;
+    }
+});
