88a70fa5d4ca705d89b6c6e671fcff74ce7110d9
jahnferdinandus <jahnferdinandus@gmail.com>
2023-12-10 18:45:01 -0500

    chart3_3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, app, jsonify, render_template
 import pandas as pd 
 
 app = Flask(__name__)
--- static/js/app3.js
+++ static/js/app3.js
@@ -1,89 +1,75 @@
-// Function to display a Leaflet map
-function chart3() {
+// Function to display a Leaflet map with markers
+function chart3(data) {
     // Create a map centered at a specific location
-    var map = L.map('graph3', {
-        center: [0, 0], // Centered at the world map
-        zoom: 3, // Initial zoom level
-        minZoom: 3, // Minimum zoom level
-        maxZoom: 4 // Maximum zoom level
-    });
+    var map = L.map('graph3').setView([20, 0], 2);
 
     // Add a tile layer to the map (using OpenStreetMap tiles)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Fetch data from chart3data.csv
-    d3.csv('/Project-3/Resources/chart3data.csv').then(function (chartData) {
-        console.log('Data:', chartData);
-        console.log('Number of Records:', chartData.length);
+    // Create a marker cluster group for better performance
+    var markers = L.markerClusterGroup();
 
-        // Extract unique years from the dataset
-        var uniqueYears = [...new Set(chartData.map(data => data.year))];
+    // Extract unique years from the data for dropdown options
+    var uniqueYears = [...new Set(data.map(item => item.year))];
 
-        // Populate the dropdown with the unique years
-        var yearDropdown = document.getElementById('yearDropdown');
-        uniqueYears.forEach(function (year) {
-            var option = document.createElement('option');
-            option.value = year;
-            option.text = year;
-            yearDropdown.add(option);
-        });
-
-        // Set up an event listener for the dropdown change event
-        yearDropdown.addEventListener('change', function () {
-            // Get the selected year from the dropdown
-            var selectedYear = yearDropdown.value;
-
-            // Filter the data based on the selected year
-            var filteredData = chartData.filter(data => data.year === selectedYear);
-
-            // Clear the existing markers on the map
-            map.eachLayer(function (layer) {
-                if (layer instanceof L.Circle) {
-                    map.removeLayer(layer);
-                }
-            });
-
-            // Loop through the filtered data and add circles to the map
-            filteredData.forEach(function (data) {
-                var color = getColorBasedOnSuicides(data['suicides/100k pop']);
-
-                // Fixed radius for all circles
-                var circle = L.circle([data.latitude, data.longitude], {
-                    color: color,
-                    fillColor: color,
-                    fillOpacity: 0.7,
-                    radius: 120000 // Fixed radius value
-                });
-
-                // Add popup with information
-                circle.bindPopup(`<b>${data.country}</b><br> Suicides/100K: ${data['suicides/100k pop']}`);
-
-                // Add the circle marker to the map
-                map.addLayer(circle);
-            });
-        });
-
-        // Initial rendering based on the first year in the dataset
-        var initialYear = uniqueYears[0];
-        yearDropdown.value = initialYear;
-        yearDropdown.dispatchEvent(new Event('change'));
+    // Add a dropdown for selecting the year
+    var yearDropdown = L.DomUtil.create('select', 'year-dropdown');
+    yearDropdown.innerHTML = '<option value="">Select Year</option>';
+    uniqueYears.forEach(year => {
+        var option = L.DomUtil.create('option');
+        option.value = year;
+        option.textContent = year;
+        yearDropdown.appendChild(option);
     });
-}
 
-// Function to determine circle color based on the number of suicides per 100K
-function getColorBasedOnSuicides(suicidesPer100k) {
-    if (suicidesPer100k < 50) {
-        return 'green';
-    } else if (suicidesPer100k < 75) {
-        return 'yellow';
-    } else {
-        return 'red';
+    // Add the dropdown to the map
+    yearDropdown.onchange = function () {
+        var selectedYear = this.value;
+        updateMarkers(selectedYear);
+    };
+    map.getContainer().appendChild(yearDropdown);
+
+    // Function to update markers based on the selected year
+    function updateMarkers(selectedYear) {
+        markers.clearLayers();
+
+        data
+            .filter(item => selectedYear === '' || item.year === selectedYear)
+            .forEach(item => {
+                // Set marker color based on the range of suicides per 100K
+                var markerColor = item['suicides/100k pop'] > 15 ? 'red' :
+                                  item['suicides/100k pop'] > 10 ? 'yellow' :
+                                  'green';
+
+                // Create a marker with a popup
+                var marker = L.circleMarker([item.latitude, item.longitude], {
+                    color: 'black',
+                    fillColor: markerColor,
+                    fillOpacity: 0.7,
+                    radius: 10
+                }).bindPopup(`
+                    <b>${item.country}</b><br>
+                    Year: ${item.year}<br>
+                    Suicides/100K: ${item['suicides/100k pop']}<br>
+                    GDP per Capita: ${item.gdp_per_capita}
+                `);
+
+                markers.addLayer(marker);
+            });
+
+        map.addLayer(markers);
     }
+
+    // Call the function initially to display all markers
+    updateMarkers('');
 }
 
-// Call the function to initialize the map when the page loads
-document.addEventListener('DOMContentLoaded', function () {
-    chart3();
-});
+// Fetch data from the API endpoint
+fetch('/api/chart3data')
+    .then(response => response.json())
+    .then(data => {
+        // Call the function to initialize the map with the fetched data
+        chart3(data);
+    });
