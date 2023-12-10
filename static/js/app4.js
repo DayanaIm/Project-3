80d3ff65b363366d09c63b4e192f47cb092dcb15
DayanaIm <dayana.imanova.cnd@gmail.com>
2023-12-10 16:22:59 -0500

    added drop down

--- static/js/app4.js
+++ static/js/app4.js
@@ -1,31 +1,58 @@
-function createInteractiveMap(data) {
-    var map = L.map('graph4',
-    {minZoom:3}
-    ).setView([55, -115], 5);
-        
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+var map = L.map('graph4', { minZoom: 3 }).setView([55, -115], 5);
 
-    data.forEach(function (location) {
-        var marker = L.marker([location.Latitude, location.Longitude]).addTo(map);
-
-        var popupContent = `<strong>${location["Provinces/Territories"]}</strong><br>
-                            Hospital Count: ${location.Hospital_count}<br>
-                            Suicides: ${location.Number_of_suicides}<br>
-                            Suicides per 100K: ${location.Number_of_suicides_per_100K}`;
-
-        marker.bindPopup(popupContent);
-    });
-}
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/api/province_data', true);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        createInteractiveMap(data);
-    }
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    var data = JSON.parse(xhr.responseText);
+    createInteractiveMap(data);
+  } else {
+    console.error('Failed to fetch data. Status:', xhr.status);
+  }
 };
 xhr.send();
+
+function createInteractiveMap(data) {
+  const uniqueYears = [...new Set(data.map(item => item.Year))];
+  const yearDropdown = document.getElementById('yearDropdown');
+
+  uniqueYears.forEach(year => {
+    const option = document.createElement('option');
+    option.value = year;
+    option.text = year;
+    yearDropdown.appendChild(option);
+  });
+
+  yearDropdown.addEventListener('change', function () {
+    const selectedYear = parseInt(this.value);
+    updateMapMarkers(data, selectedYear);
+  });
+
+  updateMapMarkers(data, uniqueYears[0]);
+}
+
+function updateMapMarkers(data, selectedYear) {
+  map.eachLayer(layer => {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+  });
+
+  const filteredData = data.filter(item => item.Year === selectedYear);
+
+  filteredData.forEach(location => {
+    const marker = L.marker([location.Latitude, location.Longitude]).addTo(map);
+
+    const popupContent = `<strong>${location["Provinces/Territories"]}</strong><br>
+                          Hospital Count: ${location.Hospital_count}<br>
+                          Suicides: ${location.Number_of_suicides}<br>
+                          Suicides per 100K: ${location.Number_of_suicides_per_100K}`;
+
+    marker.bindPopup(popupContent);
+  });
+}
